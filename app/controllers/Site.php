57e94e1d04f47c78cd17522e835e28c7ad5b71e5
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-02 18:28:36 -0300

    Passando parâmetros para o Sistema de Rotas do MVC

--- app/controllers/Site.php
+++ app/controllers/Site.php
@@ -4,8 +4,9 @@ namespace app\controllers;
 
 class Site
 {
-    public function home()
+    public function home($foto)
     {
+        $photo = $foto;
         require_once __DIR__ . '/../views/home.php';
     }
 }
--- app/views/home.php
+++ app/views/home.php
@@ -1,4 +1,6 @@
 <div class="row container">
     <div class="col s12"></div>
     <h3 class="light">Página Home</h3>
+
+    <p><?php echo $photo ?></p>
 </div>
