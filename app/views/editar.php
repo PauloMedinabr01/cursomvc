1aa03d1492555b2476b0d12974e7950590884b3c
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-02 23:40:59 -0300

    Método Update parte 2

--- app/views/editar.php
+++ app/views/editar.php
@@ -4,12 +4,27 @@
     </div>
 
     <div class="col s12">
+        <form action="?router=Site/editar/" method="post">
 
-        <?php foreach ($editarForm as $registro) : ?>
+            <?php foreach ($editarForm as $registro) : ?>
 
-            <p><?php echo $registro['nome'] ?> </p>
+                <div class="input-field col s12 m6">
+                    <input type="text" name="nome" id="nome" value="<?php echo $registro['nome'] ?>" required>
+                    <label for="nome">Digite seu nome</label>
+                </div>
 
-        <?php endforeach; ?>
+                <div class="input-field col s12 m6">
+                    <input type="email" name="email" id="email" value="<?php echo $registro['email'] ?>" required>
+                    <label for="email">Digite seu email</label>
+                </div>
 
+                <div class="input-field col s12">
+                    <input type="submit" value="salvar alterações" class="btn-small">
+                    <input type="reset" value="limpar" class="btn-small red">
+                </div>
+
+            <?php endforeach; ?>
+
+        </form>
     </div>
 </div>
